3afb9fac6b9e9590198921b5434de177fd65d4a3
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2025-02-09 22:08:16 +0700

    Test method hello
    .

---
+++ CityScout/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHello()
+    {
+        return Ok("Hello may ban test api ne hihi!");
+    }
+}
