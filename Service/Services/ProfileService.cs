ddb2dd390207fef61919edc4df0435322201ed1c
NhatDang1102 <nezukosama247@gmail.com>
2025-03-02 21:07:47 +0700

    service cho profile

---
+++ Service/Services/ProfileService.cs
@@ -0,0 +1,25 @@
+﻿using Repository.Interfaces;
+using Repository.RequestModels;  
+using Service.Interfaces;
+
+namespace Service.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IAccountRepository _accountRepository;
+        public ProfileService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<bool> UpdateProfileAsync(UpdateProfileRequest request)
+        {
+            return await _accountRepository.UpdateProfileAsync(request);
+        }
+
+        public async Task<object> GetProfileByIdAsync(int userId)
+        {
+            return await _accountRepository.GetProfileByIdAsync(userId.ToString());
+        }
+    }
+}
