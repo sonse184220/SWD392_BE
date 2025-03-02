ceae9c1e31ed5bf4337c57c7171b89db18168998
NhatDang1102 <nezukosama247@gmail.com>
2025-03-02 21:07:18 +0700

    Update AccountRepository.cs

--- Repository/Repositories/AccountRepository.cs
+++ Repository/Repositories/AccountRepository.cs
@@ -28,7 +28,37 @@ namespace Repository.Repositories
         {
             return await CreateAsync(account);
         }
+        public async Task<bool> UpdateProfileAsync(UpdateProfileRequest request)
+        {
+            var user = await _context.Accounts.FirstOrDefaultAsync(x => x.UserId == request.UserId);
+            if (user == null) return false;
 
-        
+            if (!string.IsNullOrEmpty(request.Username))
+                user.UserName = request.Username;
+            if (!string.IsNullOrEmpty(request.PhoneNumber))
+                user.PhoneNumber = request.PhoneNumber;
+            if (!string.IsNullOrEmpty(request.Address))
+                user.Address = request.Address;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<object> GetProfileByIdAsync(int userId)
+        {
+            return await _context.Accounts
+                .Where(u => u.UserId == userId.ToString()) 
+                .Select(u => new
+                {
+                    u.UserId,
+                    u.UserName,
+                    u.PhoneNumber,
+                    u.Address
+                })
+                .FirstOrDefaultAsync();
+        }
+
+
+
     }
 }
